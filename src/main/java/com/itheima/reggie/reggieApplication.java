4d1574600c0b8de910a6c521fe78b555677a68a6
fengenci <championdoggg@gmail.com>
2023-03-31 00:15:18 +0800

    项目实战

---
+++ src/main/java/com/itheima/reggie/reggieApplication.java
@@ -0,0 +1,11 @@
+package com.itheima.reggie;
+
+
+@Slf4j
+@SpringBootApplication
+public class reggieApplication {
+        public static void main(String[] args) {
+            SpringApplication.run(reggieApplication.class, args);
+            logger.info("reggieApplication is success!");
+        }
+}
